3dabcbcfd823fe21bf15cd217acb0877a15d3831
DJAINCOOL <54236855+DJAINCOOL@users.noreply.github.com>
2025-11-03 11:53:31 +0530

    Change build output directory from 'build' to 'dist'

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       build: {
-        outDir: 'build',
+        outDir: 'dist',
       },
       resolve: {
         alias: {
